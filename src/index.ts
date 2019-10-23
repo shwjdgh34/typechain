127be563b9bfd0107db4bc465cf2f1ba4a09cbeb
Macintosh HD <macintoshhd@Macintoshui-MacBook-Pro.local>
2019-10-24 00:38:06 +0900

    first blcok class

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
-const nick = new Human("nick", 27, "male");
 
-const sayHi = (person: Human): string => {
-  return `hi, im ${person.name}. i am ${person.age} years old. i am ${person.gender}`;
-};
-
-console.log(sayHi(nick));
-
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
+let blockchain: [Block] = [genesisBlock];
+console.log(blockchain);
 export {};
